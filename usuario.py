64fc836e510b25916562e2295b18f0a78fdb88b5
Matheus Cantarutti <cantaruttim@outlook.com>
2025-04-20 13:11:52 -0300

    ajuste classe Usuário

--- usuario.py
+++ usuario.py
@@ -1,6 +1,20 @@
 class Usuario:
     """
     Cria a nossa classe se usuário
+    Iremos utilizá-la para armazenar os dados do usuário
+    e senha digitados na tela de login.
+
+    Atributos:
+    user: str
+        O nome do usuário digitado na tela de login.
+    senha: str
+        A senha do usuário digitada na tela de login.
+        
+    Métodos:
+    getUser: str
+        Retorna o nome do usuário digitado na tela de login.
+    getSenha: str
+        Retorna a senha do usuário digitada na tela de login.
     """
     def __init__(self, user, senha):
         self.user = user
